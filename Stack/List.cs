6d78fd0f9d95ba304de6c36a23b93779b61f4c7b
Andrey Drobozin <crash1601@gmail.com>
2018-02-13 15:35:02 +0300

    13_02

---
+++ Stack/List.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class List<T>
+        where T : ICloneable
+    {
+        private T def = default(T);
+        Node<T> head;
+        public int count { get; private set; }
+        private string mes_em = "Stack is Empty.";
+
+       public void Push(T data)
+        {
+            Node<T> temp = new Node<T>(data);
+            temp.Next = head;
+            head = temp;
+            ++count;
+        }
+        public T Pop()
+        {
+            if (count == 0) Console.WriteLine(mes_em);
+            else
+            {
+                Node<T> temp = head;
+                head = head.Next;
+                --count;
+                return temp.Data;
+            }
+            return def;
+        }
+        public T Top()
+        {
+            if (count == 0) Console.WriteLine(mes_em);
+            else 
+            {
+                return (T) head.Data.Clone();
+            }
+            return def;
+        }
+        public void Print()
+        {
+            Node<T> temp = head;
+            if (count == 0) Console.WriteLine(mes_em);
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Data);
+                temp = temp.Next;
+            }
+        }
+
+        public class Node<TElement>
+        {
+            public TElement Data { get; set; }
+            public Node<TElement> Next { get; set; }
+            public Node(TElement data)
+            {
+                Data = data;
+            }
+ 
+        }
+    }
+}
+
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -10,6 +10,25 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Stack: ");
+            List<string> stringStack = new List<string>();
+            for (int i = 0; i < 5; ++i)
+            {
+                stringStack.Push(Convert.ToString(i));
+            }
+            stringStack.Print();
+            Console.WriteLine("\n");
+            Console.WriteLine($"First value: {stringStack.Top()}");
+            for (int i = stringStack.count; i > 0; --i)
+            {
+                stringStack.Pop();
+            }
+
+            Console.WriteLine("Stack after Pop: ");
+            stringStack.Print();
+        
+
+            Console.ReadLine();
         }
     }
 }
